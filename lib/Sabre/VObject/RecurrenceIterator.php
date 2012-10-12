632f914c26b4fb9341744909953c5b94084b44c6
Michael Holm <hollo@hollo.dk>
2012-10-12 15:28:24 +0200

    Chenged the hour output format.

--- lib/Sabre/VObject/RecurrenceIterator.php
+++ lib/Sabre/VObject/RecurrenceIterator.php
@@ -705,7 +705,7 @@ class RecurrenceIterator implements \Iterator {
             $this->currentDate->modify('+' . $this->interval . ' hours');
 
             // Current hour of the day
-            $currentHour = $this->currentDate->format('H');
+            $currentHour = $this->currentDate->format('G');
 
         } while (!in_array($currentHour, $recurrenceHours));
 
