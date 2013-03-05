294831a24b8484a7330d98812bfe2f256f830805
Evert Pot <evert@rooftopsolutions.nl>
2013-03-05 19:25:16 +0100

    Not sure how I missed these CS problems.

--- lib/Sabre/VObject/Property/Compound.php
+++ lib/Sabre/VObject/Property/Compound.php
@@ -5,45 +5,41 @@ namespace Sabre\VObject\Property;
 use Sabre\VObject;
 
 /**
-* Compound property.
-*
-* This class adds (de)serialization of compound properties to/from arrays.
-*
-* Currently the following properties from RFC 6350 are mapped to use this
-* class:
-*
-*  N:          Section 6.2.2
-*  ADR:        Section 6.3.1
-*  ORG:        Section 6.6.4
-*  CATEGORIES: Section 6.7.1
-*
-* In order to use this correctly, you must call setParts and getParts to
-* retrieve and modify dates respectively.
-*
-* @author Thomas Tanghus (http://tanghus.net/)
-* @author Lars Kneschke
-* @author Evert Pot (http://www.rooftopsolutions.nl/)
-* @copyright Copyright (C) 2007-2013 Rooftop Solutions. All rights reserved.
-* @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
-*/
-
-/**
-* This class represents a compound property in a vCard.
-*/
+ * Compound property.
+ *
+ * This class adds (de)serialization of compound properties to/from arrays.
+ *
+ * Currently the following properties from RFC 6350 are mapped to use this
+ * class:
+ *
+ *  N:          Section 6.2.2
+ *  ADR:        Section 6.3.1
+ *  ORG:        Section 6.6.4
+ *  CATEGORIES: Section 6.7.1
+ *
+ * In order to use this correctly, you must call setParts and getParts to
+ * retrieve and modify dates respectively.
+ *
+ * @author Thomas Tanghus (http://tanghus.net/)
+ * @author Lars Kneschke
+ * @author Evert Pot (http://www.rooftopsolutions.nl/)
+ * @copyright Copyright (C) 2007-2013 Rooftop Solutions. All rights reserved.
+ * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
+ */
 class Compound extends VObject\Property {
 
     /**
-    * If property names are added to this map, they will be (de)serialised as arrays
-    * using the getParts() and setParts() methods.
-    * The keys are the property names, values are delimiter chars.
-    *
-    * @var array
-    */
+     * If property names are added to this map, they will be (de)serialised as arrays
+     * using the getParts() and setParts() methods.
+     * The keys are the property names, values are delimiter chars.
+     *
+     * @var array
+     */
     static public $delimiterMap = array(
-        'N'				=>	';',
-        'ADR'			=>	';',
-        'ORG'			=>	';',
-        'CATEGORIES'	=>	',',
+        'N'           =>    ';',
+        'ADR'         =>    ';',
+        'ORG'         =>    ';',
+        'CATEGORIES'  =>    ',',
     );
 
     /**
@@ -54,11 +50,11 @@ class Compound extends VObject\Property {
     protected $delimiter = null;
 
     /**
-    * Get a compound value as an array.
-    *
-    * @param $name string
-    * @return array
-    */
+     * Get a compound value as an array.
+     *
+     * @param $name string
+     * @return array
+     */
     public function getParts() {
 
         if (is_null($this->value)) {
@@ -108,10 +104,10 @@ class Compound extends VObject\Property {
     /**
      * Set a compound value as an array.
      *
-    *
-    * @param $name string
-    * @return array
-    */
+     *
+     * @param $name string
+     * @return array
+     */
     public function setParts(array $values) {
 
         // add slashes to all semicolons and commas in the single values
