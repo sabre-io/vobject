fe20656023c5ab1bf1e01089ecff85a03fc3bc64
Evert Pot <evert@rooftopsolutions.nl>
2013-03-21 23:20:33 +0000

    We are still on PHP 5.3 here.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-$try = [
+$try = array(
     __DIR__ . '/../vendor/autoload.php',
     __DIR__ . '/../../../autoload.php',
-];
+);
 
 foreach($try as $path) {
     if (file_exists($path)) {
