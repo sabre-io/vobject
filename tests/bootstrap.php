116ae8cd1cf12e3bbf4cce8c093035de19d83b19
Evert Pot <evert@rooftopsolutions.nl>
2013-03-21 23:17:55 +0000

    Making sure unittests can also run when vobject is a composer dependency.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,13 @@
 <?php
 
-// Composer autoloader
-include __DIR__ . '/../vendor/autoload.php';
+$try = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+];
+
+foreach($try as $path) {
+    if (file_exists($path)) {
+        include $path;
+        break;
+    }
+}
