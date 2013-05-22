4e9730ffaa6f87f5548c2b068c02047b51340768
Evert Pot <evert@rooftopsolutions.nl>
2013-05-22 19:32:06 +0100

    STABILITY constant had been removed

--- tests/Sabre/VObject/VersionTest.php
+++ tests/Sabre/VObject/VersionTest.php
@@ -7,10 +7,7 @@ class VersionTest extends \PHPUnit_Framework_TestCase {
     function testString() {
 
         $v = Version::VERSION;
-        $this->assertEquals(-1, version_compare('0.9.0',$v));
-
-        $s = Version::STABILITY;
-        $this->assertTrue($s == 'alpha' || $s == 'beta' || $s =='stable');
+        $this->assertEquals(-1, version_compare('2.0.0',$v));
 
     }
 
