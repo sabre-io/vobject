37f4bd809c4a5cdf5002bf15aa8428a5240745f2
Evert Pot <evert@rooftopsolutions.nl>
2014-01-22 21:02:27 -0500

    Catching a more specific exception. Issue #74.

--- lib/Sabre/VObject/RecurrenceIterator.php
+++ lib/Sabre/VObject/RecurrenceIterator.php
@@ -467,7 +467,7 @@ class RecurrenceIterator implements \Iterator {
                     try {
                         $this->exceptionDates[] =
                             DateTimeParser::parse($exceptionDate, $this->startDate->getTimeZone());
-                    } catch (Exception $e) {
+                    } catch (\LogicException $e) {
                     }
 
                 }
