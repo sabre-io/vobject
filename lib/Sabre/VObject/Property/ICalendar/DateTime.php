fa2459ea0e7ecc2ac12595730046de49411cb3b0
Evert Pot <evert@rooftopsolutions.nl>
2014-01-22 20:29:45 -0500

    Happy 2014!

--- lib/Sabre/VObject/Property/ICalendar/DateTime.php
+++ lib/Sabre/VObject/Property/ICalendar/DateTime.php
@@ -19,7 +19,7 @@ use
  * cases represent a DATE value. This is because it's a common usecase to be
  * able to change a DATE-TIME into a DATE.
  *
- * @copyright Copyright (C) 2007-2013 fruux GmbH. All rights reserved.
+ * @copyright Copyright (C) 2007-2014 fruux GmbH. All rights reserved.
  * @author Evert Pot (http://evertpot.com/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
