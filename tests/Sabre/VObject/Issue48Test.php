975a515d3f657258a69de621df6b833457b13392
Evert Pot <evert@rooftopsolutions.nl>
2014-08-06 17:56:16 -0400

    PHP 5.3

--- tests/Sabre/VObject/Issue48Test.php
+++ tests/Sabre/VObject/Issue48Test.php
@@ -36,11 +36,11 @@ ICS;
 
         $tz = new DateTimeZone('Europe/Moscow');
 
-        $expected = [
+        $expected = array(
             new DateTime('2013-07-10 11:00:00', $tz),
             new DateTime('2013-07-12 11:00:00', $tz),
             new DateTime('2013-07-13 11:00:00', $tz),
-        ];
+        );
 
         $this->assertEquals($expected, $result);
 
