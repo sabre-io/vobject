807e1f417a028428a37a135fc21b797793a15b61
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-11-11 17:19:34 +0100

    !xml Early early draft.

---
+++ lib/Parser/XML.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sabre\VObject\Parser;
+
+use
+    Sabre\VObject\Component\VCalendar,
+    Sabre\VObject\Component\VCard;
+
+/**
+ * XML Parser.
+ *
+ * This parser parses both the xCal and xCard formats.
+ *
+ * @copyright Copyright (C) 2007-2014 fruux GmbH. All rights reserved.
+ * @author Ivan Enderlin
+ * @license http://sabre.io/license/ Modified BSD License
+ */
+class XML extends Parser {
+
+
+}
