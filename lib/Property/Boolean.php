9d9bcfeee120d124ca96cca5a7fb17a8741ebf5f
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-11-18 11:25:55 +0100

    !xml <boolean>

--- lib/Property/Boolean.php
+++ lib/Property/Boolean.php
@@ -60,4 +60,24 @@ class Boolean extends Property {
 
     }
 
+    /**
+     * Sets the XML value, as it would appear in a xCard or xCal object.
+     *
+     * The value must always be an array.
+     *
+     * @param array $value
+     * @return void
+     */
+    function setXmlValue(array $value) {
+
+        $value = array_map(
+            function($value) {
+                return 'true' === $value;
+            },
+            $value
+        );
+        $this->setXmlValue($value);
+
+    }
+
 }
