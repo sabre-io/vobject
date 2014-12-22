969afea733012415c1662d90e735227e3b30e746
Dominik Tobschall <dominik@fruux.com>
2014-12-22 14:28:17 +0100

    test for vcard converter issue

---
+++ tests/VObject/IssueXXTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sabre\VObject;
+
+class IssueXXTest extends \PHPUnit_Framework_TestCase {
+
+  function testConvert() {
+
+    $input = <<<VCF
+BEGIN:VCARD
+VERSION:3.0
+UID:foo
+N:Doe;John;;;
+FN:John Doe
+item1.X-ABDATE;type=pref:2008-12-11
+END:VCARD
+
+VCF;
+
+    $vcard = Reader::read($input);
+
+    $this->assertInstanceOf('Sabre\\VObject\\Component\\VCard', $vcard);
+    $vcard = $vcard->convert(\Sabre\VObject\Document::VCARD40);
+    $vcard = $vcard->serialize();
+
+    $converted = Reader::read($vcard);
+    $converted->validate();
+
+    $version = Version::VERSION;
+
+    $expected = <<<VCF
+BEGIN:VCARD
+VERSION:4.0
+PRODID:-//Sabre//Sabre VObject $version//EN
+UID:foo
+N:Doe;John;;;
+FN:John Doe
+ITEM1.X-ABDATE;PREF=1:2008-12-11
+END:VCARD
+
+VCF;
+
+    $this->assertEquals($expected, str_replace("\r","", $vcard));
+
+  }
+
+}
