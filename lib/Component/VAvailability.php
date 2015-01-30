829b7078e10c37da7c5252f6ba29f5fd30d5f4fd
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-01-30 11:53:17 +0100

    Add validation rules.

--- lib/Component/VAvailability.php
+++ lib/Component/VAvailability.php
@@ -16,4 +16,45 @@ use Sabre\VObject;
  */
 class VAvailability extends VObject\Component {
 
+    /**
+     * A simple list of validation rules.
+     *
+     * This is simply a list of properties, and how many times they either
+     * must or must not appear.
+     *
+     * Possible values per property:
+     *   * 0 - Must not appear.
+     *   * 1 - Must appear exactly once.
+     *   * + - Must appear at least once.
+     *   * * - Can appear any number of times.
+     *   * ? - May appear, but not more than once.
+     *
+     * @var array
+     */
+    function getValidationRules() {
+
+        return [
+            'UID' => 1,
+            'DTSTAMP' => 1,
+
+            'BUSYTYPE' => '?',
+            'CLASS' => '?',
+            'CREATED' => '?',
+            'DESCRIPTION' => '?',
+            'DTSTART' => '?',
+            'LAST-MODIFIED' => '?',
+            'ORGANIZER' => '?',
+            'PRIORITY' => '?',
+            'SEQUENCE' => '?',
+            'SUMMARY' => '?',
+            'URL' => '?',
+            'DTEND' => '?',
+            'DURATION' => '?',
+
+            'CATEGORIES' => '*',
+            'COMMENT' => '*',
+            'CONTACT' => '*',
+        ];
+
+    }
 }
