6691ee661c6b8ffb1f283f764392f017c673b141
Renaud BOYER <rboyer@linagora.com>
2018-03-28 16:41:11 +0200

    linagora/lgs/lgs-projects/Industrialisation_V1#166: Compare date timestamp

--- lib/ITip/Broker.php
+++ lib/ITip/Broker.php
@@ -436,9 +436,9 @@ class Broker
             // Use the Unix timestamp returned by getTimestamp as a unique identifier for the recurrence.
             // The Unix timestamp will be the same for an event, even if the reply from the attendee
             // used a different format/timezone to express the event date-time.
-            $recurId = isset($vevent->{'RECURRENCE-ID'}) ? $vevent->{'RECURRENCE-ID'}->getDateTime()->getTimestamp() : 'master';
+            $recurIdTimestamp = isset($vevent->{'RECURRENCE-ID'}) ? $vevent->{'RECURRENCE-ID'}->getDateTime()->getTimestamp() : 'master';
             $attendee = $vevent->ATTENDEE;
-            $instances[$recurId] = [
+            $instances[$recurIdTimestamp] = [
                 'partstat'        => $attendee['PARTSTAT']->getValue(),
                 'recurIdDateTime' => isset($vevent->{'RECURRENCE-ID'}) ? $vevent->{'RECURRENCE-ID'}->getDateTime() : null
             ];
@@ -453,17 +453,17 @@ class Broker
         $masterObject = null;
         foreach ($existingObject->VEVENT as $vevent) {
             // Use the Unix timestamp returned by getTimestamp as a unique identifier for the recurrence.
-            $recurId = isset($vevent->{'RECURRENCE-ID'}) ? $vevent->{'RECURRENCE-ID'}->getDateTime()->getTimestamp() : 'master';
-            if ('master' === $recurId) {
+            $recurIdTimestamp = isset($vevent->{'RECURRENCE-ID'}) ? $vevent->{'RECURRENCE-ID'}->getDateTime()->getTimestamp() : 'master';
+            if ('master' === $recurIdTimestamp) {
                 $masterObject = $vevent;
             }
-            if (isset($instances[$recurId])) {
+            if (isset($instances[$recurIdTimestamp])) {
                 $attendeeFound = false;
                 if (isset($vevent->ATTENDEE)) {
                     foreach ($vevent->ATTENDEE as $attendee) {
                         if ($attendee->getValue() === $itipMessage->sender) {
                             $attendeeFound = true;
-                            $attendee['PARTSTAT'] = $instances[$recurId]['partstat'];
+                            $attendee['PARTSTAT'] = $instances[$recurIdTimestamp]['partstat'];
                             $attendee['SCHEDULE-STATUS'] = $requestStatus;
                             // Un-setting the RSVP status, because we now know
                             // that the attendee already replied.
@@ -476,13 +476,13 @@ class Broker
                     // Adding a new attendee. The iTip documentation calls this
                     // a party crasher.
                     $attendee = $vevent->add('ATTENDEE', $itipMessage->sender, [
-                        'PARTSTAT' => $instances[$recurId]['partstat']
+                        'PARTSTAT' => $instances[$recurIdTimestamp]['partstat']
                     ]);
                     if ($itipMessage->senderName) {
                         $attendee['CN'] = $itipMessage->senderName;
                     }
                 }
-                unset($instances[$recurId]);
+                unset($instances[$recurIdTimestamp]);
             }
         }
 
@@ -492,11 +492,10 @@ class Broker
         }
         // If we got replies to instances that did not exist in the
         // original list, it means that new exceptions must be created.
-        foreach ($instances as $recurId => $instance) {
+        foreach ($instances as $recurIdTimestamp => $instance) {
             $recurrenceIterator = new EventIterator($existingObject, $itipMessage->uid);
             $found = false;
             $iterations = 1000;
-            $recurIdDate = $instance['recurIdDateTime'];
             do {
                 $newObject = $recurrenceIterator->getEventObject();
                 $recurrenceIterator->next();
@@ -504,7 +503,7 @@ class Broker
                 // Compare the Unix timestamp returned by getTimestamp with the previously calculated timestamp.
                 // If they are the same, then this is a matching recurrence, even though its date-time may have
                 // been expressed in a different format/timezone.
-                if (isset($newObject->{'RECURRENCE-ID'}) && $newObject->{'RECURRENCE-ID'}->getDateTime()->getTimestamp() === $recurId) {
+                if (isset($newObject->{'RECURRENCE-ID'}) && $newObject->{'RECURRENCE-ID'}->getDateTime()->getTimestamp() === $recurIdTimestamp) {
                     $found = true;
                 }
                 --$iterations;
