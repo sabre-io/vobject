2d30f2cc8cab38745713d91a256c02aebd70b12a
Aleksander Machniak <machniak@kolabsys.com>
2018-09-25 09:52:42 +0200

    Use `break` instead of `continue 2`

--- lib/FreeBusyGenerator.php
+++ lib/FreeBusyGenerator.php
@@ -430,7 +430,7 @@ class FreeBusyGenerator {
                                 // instance. We are skipping this event from the output
                                 // entirely.
                                 unset($this->objects[$key]);
-                                continue 2;
+                                break;
                             }
 
                             if ($this->start) {
