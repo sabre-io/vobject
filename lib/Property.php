f3911db4a54c64c6c8977644e3020ba5dd32e714
Weby <weby@we-bb.com>
2018-12-24 11:55:13 +0100

    Prettify code

--- lib/Property.php
+++ lib/Property.php
@@ -573,16 +573,14 @@ abstract class Property extends Node
                     case Document::VCARD30:
                         $allowedEncoding = ['B'];
                         //Repair vCard30 that use BASE64 encoding
-                        if($options & self::REPAIR)
-                        {
-                            if(strtoupper($encoding)=="BASE64")
-                            {
-                                $encoding="B";
-                                $this->offsetSet('ENCODING',$encoding);
+                        if($options & self::REPAIR) {
+                            if(strtoupper($encoding)=="BASE64") {
+                                $encoding = 'B';
+                                $this->offsetSet('ENCODING', $encoding);
                                 $warnings[] = [
                                     'level' => 1,
                                     'message' => 'ENCODING=BASE64 has been transformed to ENCODING=B.',
-                                    'node'=>$this,
+                                    'node' => $this,
                                 ];
                             }
                         }
