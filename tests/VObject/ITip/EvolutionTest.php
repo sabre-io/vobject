94b5abecb0987faf4a3ab8001d768b8a1887bcd4
Alexander Ivannikov <alexander.ivannikov@dsr-company.com>
2019-07-04 19:09:38 +0300

    Fixes for unit tests

--- tests/VObject/ITip/EvolutionTest.php
+++ tests/VObject/ITip/EvolutionTest.php
@@ -844,6 +844,7 @@ END:STANDARD
 END:VTIMEZONE
 BEGIN:VEVENT
 UID:20140813T153116Z-12176-1000-1065-6@johnny-lubuntu
+DTSTAMP:**ANY**
 DTSTART;TZID=/freeassociation.sourceforge.net/Tzfile/America/Toronto:201408
  15T110000
 DTEND;TZID=/freeassociation.sourceforge.net/Tzfile/America/Toronto:20140815
