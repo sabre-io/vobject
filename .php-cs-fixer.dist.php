f57617769d950d40b5908d0444ab58f53eb10ce9
Phil Davis <phil@jankaritech.com>
2022-08-23 14:03:27 +0545

    Add .php-cs-fixer.dist.php to itself

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,10 +3,13 @@
 $config = new PhpCsFixer\Config();
 $config->getFinder()
     ->exclude('vendor')
-    ->in(__DIR__);
+    ->in(__DIR__)
+    ->append([
+        __FILE__,
+    ]);
 $config->setRules([
     '@PSR1' => true,
-    '@Symfony' => true
+    '@Symfony' => true,
 ]);
 
 return $config;
