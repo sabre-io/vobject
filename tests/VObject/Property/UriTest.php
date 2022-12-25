169e20b5a371e9ecd2c17cef4bd1568e05c7c206
Michael Stilkerich <ms@mike2k.de>
2022-12-25 08:48:36 +0100

    Unescape also semicolon in URI Property
    This is required for VCard4 (see test sample), as comma and semicolon
    may appear in URIs with scheme data.

--- tests/VObject/Property/UriTest.php
+++ tests/VObject/Property/UriTest.php
@@ -23,4 +23,29 @@ ICS;
         $output = Reader::read($input)->serialize();
         $this->assertStringContainsString('URL;VALUE=URI:http://example.org/', $output);
     }
+
+    public function testUriUnescapedProperly(): void
+    {
+        // The colon should normally not be escaped in URL, but Google Contacts does it and
+        // vobject contains a workaround for it
+        $input = <<<VCF
+BEGIN:VCARD
+VERSION:4.0
+PRODID:-//Thunderbird.net/NONSGML Thunderbird CardBook V83.6//EN-GB
+UID:5cdac90f-cb4c-4c1c-ad66-fe8591bdf3a2
+FN:vCard 4 Contact with image
+EMAIL:bbb@example.org
+REV:20221222T213003Z
+PHOTO:data:image/JPEG\;base64\,/9j/4AAQSkZJRgABAQAAYABgAAD/2wBDAAgGBgcGBQgHB
+ wcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/
+ wAALCAABAAEBAREA/8QAFAABAAAAAAAAAAAAAAAAAAAAAv/EABQQAQAAAAAAAAAAAAAAAAAAAAD
+ /2gAIAQEAAD8AL//Z
+item1.URL:http\://www.example.com/hello?world
+item1.X-ABLabel:
+END:VCARD
+VCF;
+        $output = Reader::read($input);
+        $this->assertSame('http://www.example.com/hello?world', (string) $output->URL);
+        $this->assertSame('data:image/JPEG;base64,/9j/4AAQSkZJRgABAQAAYABgAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/wAALCAABAAEBAREA/8QAFAABAAAAAAAAAAAAAAAAAAAAAv/EABQQAQAAAAAAAAAAAAAAAAAAAAD/2gAIAQEAAD8AL//Z', (string) $output->PHOTO);
+    }
 }
