3bfeaee59202913f4a8b476a83a763d99ca34975
Christoph Wurst <christoph@winzerhof-wurst.at>
2024-09-17 21:25:09 +0200

    refactor: Replace dirname(__FILE__) with __DIR__
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- tests/VObject/Issue64Test.php
+++ tests/VObject/Issue64Test.php
@@ -8,7 +8,7 @@ class Issue64Test extends TestCase
 {
     public function testRead(): void
     {
-        $vcard = Reader::read(file_get_contents(dirname(__FILE__).'/issue64.vcf'));
+        $vcard = Reader::read(file_get_contents(__DIR__.'/issue64.vcf'));
         $vcard = $vcard->convert(Document::VCARD30);
         $vcard = $vcard->serialize();
 
